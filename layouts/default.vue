56b49f76345c3741a05e094d9e2cfb5c70fde347
molleahahs <1593576673@qq.com>
2020-03-07 23:31:45 +0800

    注册，登录，推出 smtp

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-container class="layout-default">
+    <el-header><my-header/></el-header>
+    <el-main>
+      <nuxt />
+    </el-main>
+    <el-footer height="100%"><my-footer/></el-footer>
+  </el-container>
+</template>
+
+<script>
+  import MyHeader from '@/components/public/header/index.vue'
+  import MyFooter from '@/components/public/footer/index.vue'
+  export default {
+    components: {
+      MyHeader, MyFooter
+    }
+  }
+</script>
+<style>
+</style>
