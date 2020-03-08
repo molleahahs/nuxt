7fd8e013dcc7eb8e47f00584e52575537f0b2a5f
molleahahs <1593576673@qq.com>
2020-03-08 13:58:24 +0800

    头部覆盖element默认60高，使样式正常

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <el-container class="layout-default">
-    <el-header><my-header/></el-header>
+    <el-header height="200px"><my-header/></el-header>
     <el-main>
       <nuxt />
     </el-main>
